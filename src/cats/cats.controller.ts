e3c7667048d9a713c6b906f984d20aee84622623
lavard <1095041759@qq.com>
2022-02-23 11:27:10 +0800

    实现本地数据crud

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,11 +1,15 @@
-import { Body, Controller, Get, Param, Patch, Post, Res } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Res } from '@nestjs/common';
 import { response } from 'express';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+    constructor(private readonly catsService:CatsService){}
+
     @Get()
-    findAll(): string {
-        return 'This action returns all cats';
+    findAll(@Query() paginationQuery): string {
+        const {limit,offset} = paginationQuery;
+        return `This action returns all cats.Limit: ${limit}, offset: ${offset}`;
     }
 
     @Get(':id')
@@ -20,6 +24,13 @@ export class CatsController {
 
     @Patch(':id')
     update(@Param('id') id:string, @Body() body){
-        
+        return `this action update #${id} coffee;`    
     }
+
+    @Delete(':id')  
+    remove(@Param('id') id:string){
+        return `this action remove #${id} coffee;`
+    }
+
+
 }
